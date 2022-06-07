2956e1648810d5ab89adac5aa45d2516bf14f251
Raul Munoz <107068638+raulipvg@users.noreply.github.com>
2022-06-07 13:45:26 -0400

    Proy inicial
    Blabla bla inicial v1

---
+++ palGit/palGit/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el primer numero");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo numero");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("{0} + {1} = {2}",a,b,a+b);
+
+            Console.ReadLine();
+
+        }
+    }
+}
